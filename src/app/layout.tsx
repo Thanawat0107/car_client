9db18fff50f0d14e967123ce3da199f7fc4f91ad
gnek.y <qwwasszxx2345@gmail.com>
2026-03-15 23:25:32 +0700

    เตรียมปรับปรุง car

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navber from "@/components/layouts/Navber";
 import Footer from "@/components/layouts/Footer";
-import { Providers } from "./Providers";
+import { Providers } from "./providers";
 import AuthProvider from "./AuthProvider";
 
 const geistSans = Geist({
--- src/utility/SD.ts
+++ src/utility/SD.ts
@@ -2,6 +2,6 @@
 
 // export const baseUrlAPI = baseUrl + "/api/";
 
-export const baseUrl = "https://localhost:7001";
+export const baseUrl = "http://localhost:5178";
 
 export const baseUrlAPI = baseUrl + "/api/";
